54a3592c7db367f16820e7a34d11308d04585ea5
Victroria Anisimova <vitkakim@mail.ru>
2021-12-18 22:33:43 +0300

    4th HW

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,36 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined){ 
+    this.marks = [mark];
+    } else {
+      this.marks.push(mark);
+    }
+}
+
+Student.prototype.addMarks = function (...mark) {
+  if(this.marks === undefined){ 
+    this.marks = [...mark];
+    } else {
+      this.marks.push(...mark);
+    }
+}
+
+Student.prototype.getAverage = function () {
+  sum = this.marks.reduce((acc, element) => acc + element, 0);
+  return sum / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.marks;
+  delete this.subject;
+  this.excluded = reason;
+}
