38693effef6f56d6b1b95b061b2bcb58394a86aa
Victroria Anisimova <vitkakim@mail.ru>
2021-12-18 18:29:24 +0300

    3rd HW

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,18 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
-  return result; // boolean
+   if (arr1.length == arr2.length && arr1.every((value, index) => value === arr2[index])) {
+     result = true;
+   } else {
+    result = false;
+   }
+  
+  return result;
 }
 
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  resultArr = arr.filter((item) => item % 3 === 0).filter((item) => item >= 0).map((item) => item * 10);
 
-  return resultArr; // array
+  return resultArr;
 }
