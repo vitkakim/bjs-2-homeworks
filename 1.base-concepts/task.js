ac17a5eb971990643060bf47edcf6aa35d83c947
Victroria Anisimova <vitkakim@mail.ru>
2021-12-18 13:00:15 +0300

    1s HW 1st task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,14 @@
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let arr = [];
+  let discriminant = Math.pow(b, 2) - 4 * a * c;
+  if (discriminant == 0){
+    arr.push(- b / (2 * a));
+  }
+  if (discriminant > 0){
+    arr.push(((- b + Math.sqrt(discriminant)) / (2 * a)).toFixed(3) );
+    arr.push(((-b - Math.sqrt(discriminant)) / (2 * a)).toFixed(3) );
+  }
+  return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
